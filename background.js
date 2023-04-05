463f1acab7d2de33e1fa070030e3b777e1d6a23f
jdoe-gce <129967429+jdoe-gce@users.noreply.github.com>
2023-04-05 17:57:46 +0200

    move redirect function inside async get stored values

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 var _groups = undefined;
-var _url = undefined;
 
 function sortOnKeys(dict) {
     var sorted = [];
@@ -16,45 +15,51 @@ function sortOnKeys(dict) {
     return tempDict;
 }
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	var group = undefined;
+function redirectToNewURL(url){
+	const regex = /^https?:\/\//i;
+	var _url    = undefined;
+	var group   = undefined;
+
+	for(var key in _groups){
+		if(_groups[key]['active']){
+			group = key;
+			break;
+		}
+	}
 	
+	// in case of no active rules group or other undefined value
+	if(group === undefined ||
+		_groups[group]['rules'] === null || 
+		_groups[group]['rules'] === undefined) return;
+	
+	for(i = 0 ; i < _groups[group]['rules'].length ; i++){
+		// split with "/" (if long URL) then with ":" (if port)
+		var res = url.replace(regex,'').split('/')[0].split(':')[0];
+		
+		if(res == _groups[group]['rules'][i][0].split('/')[0]){
+			_url = _groups[group]['rules'][i][1].split('/')[0];
+			return url.replace(res, _url);
+		}				
+	}
+	
+	return undefined;
+}
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	chrome.storage.sync.get("groups", function(items){
 		for(var key in items){
 			if(key == "groups"){
 				_groups = sortOnKeys(items[key]);
+				if ((changeInfo.url || tab.url) && _groups !== undefined) {
+					var url = changeInfo.url ? changeInfo.url : tab.url;
+					var res = redirectToNewURL(url);
+				}
+				if(res){
+					console.log("Redirect from '" + url + "' to '" + res + "'");
+					chrome.tabs.update(tabId, { url: res });
+				}
 				break;
 			}
 		}
-	});
-	
-	if ((changeInfo.url || tab.url) && _groups !== undefined) {
-	
-		var url = changeInfo.url ? changeInfo.url : tab.url;
-		
-		for(var key in _groups){
-			if(_groups[key]['active']){
-				group = key;
-				break;
-			}
-		}
-		
-		if(group === undefined) return; // in case of no active rules group
-		
-		if(_groups[group]['rules'] === null || 
-			_groups[group]['rules'] === undefined)
-			return;
-		
-		const regex = /^https?:\/\//i;
-		
-		for(i = 0 ; i < _groups[group]['rules'].length ; i++){
-			var res = url.replace(regex,'').split('/')[0].split(':')[0];
-			
-			if(res == _groups[group]['rules'][i][0].split('/')[0]){
-				_url = _groups[group]['rules'][i][1].split('/')[0];
-				console.log("Redirect from '"+url+"' to '"+url.replace(res, _url)+"'");
-				chrome.tabs.update(tabId, { url: url.replace(res, _url) });
-			}				
-		}
-	}
+	});	
 });
